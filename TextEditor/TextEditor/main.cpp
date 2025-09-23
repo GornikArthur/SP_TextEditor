8a10392d01cf92cefa9bab02a4c204d47be6b9da
artur <arturs.gorniks@gmail.com>
2025-09-23 19:54:20 +0300

    23.09

--- TextEditor/TextEditor/main.cpp
+++ TextEditor/TextEditor/main.cpp
@@ -134,12 +134,90 @@ void ShowScreensaver()
     UpdateWindow(hwndSaver);
 }
 
+void LoadFileToEdit(HWND hwndEdit, LPCWSTR fileName) {
+    HANDLE hFile = CreateFileW(fileName, GENERIC_READ, FILE_SHARE_READ, NULL,
+        OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE) return;
+
+    DWORD fileSize = GetFileSize(hFile, NULL);
+    if (fileSize == INVALID_FILE_SIZE) { CloseHandle(hFile); return; }
+
+    char* buffer = (char*)HeapAlloc(GetProcessHeap(), 0, fileSize + 1);
+    if (!buffer) { CloseHandle(hFile); return; }
+
+    DWORD bytesRead;
+    if (ReadFile(hFile, buffer, fileSize, &bytesRead, NULL)) {
+        buffer[bytesRead] = '\0';
+
+        // преобразуем в Unicode (здесь предполагаем UTF-8)
+        int lenW = MultiByteToWideChar(CP_UTF8, 0, buffer, -1, NULL, 0);
+        if (lenW > 0) {
+            wchar_t* wbuf = (wchar_t*)HeapAlloc(GetProcessHeap(), 0, lenW * sizeof(wchar_t));
+            if (wbuf) {
+                MultiByteToWideChar(CP_UTF8, 0, buffer, -1, wbuf, lenW);
+                SetWindowTextW(hwndEdit, wbuf);
+                HeapFree(GetProcessHeap(), 0, wbuf);
+            }
+        }
+    }
+    HeapFree(GetProcessHeap(), 0, buffer);
+    CloseHandle(hFile);
+}
+
+void SaveEditToFile(HWND hwndEdit, LPCWSTR fileName) {
+    int len = GetWindowTextLengthW(hwndEdit);
+    wchar_t* buffer = (wchar_t*)HeapAlloc(GetProcessHeap(), 0, (len + 1) * sizeof(wchar_t));
+    if (!buffer) return;
+
+    GetWindowTextW(hwndEdit, buffer, len + 1);
+
+    // Конвертируем в UTF-8
+    int utf8len = WideCharToMultiByte(CP_UTF8, 0, buffer, -1, NULL, 0, NULL, NULL);
+    char* utf8buffer = (char*)HeapAlloc(GetProcessHeap(), 0, utf8len);
+    if (!utf8buffer) { HeapFree(GetProcessHeap(), 0, buffer); return; }
+
+    WideCharToMultiByte(CP_UTF8, 0, buffer, -1, utf8buffer, utf8len, NULL, NULL);
+
+    HANDLE hFile = CreateFileW(fileName, GENERIC_WRITE, 0, NULL,
+        CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile != INVALID_HANDLE_VALUE) {
+        DWORD bytesWritten;
+        WriteFile(hFile, utf8buffer, utf8len - 1, &bytesWritten, NULL); // без \0
+        CloseHandle(hFile);
+    }
+
+    HeapFree(GetProcessHeap(), 0, buffer);
+    HeapFree(GetProcessHeap(), 0, utf8buffer);
+}
+
+HWND hEdit = NULL;
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     switch (msg)
     {
     case WM_CREATE:
     {
+
+        hEdit = CreateWindowExW(
+            0,                      // dwExStyle
+            L"EDIT",                // класс
+            L"",                    // начальный текст
+            WS_CHILD | WS_VISIBLE | WS_BORDER |
+            ES_LEFT | ES_MULTILINE |
+            ES_AUTOVSCROLL | ES_AUTOHSCROLL |
+            ES_WANTRETURN,          // ES_WANTRETURN полезен в диалогах/окнах, чтобы Enter вставлялся
+            10, 10, 760, 520,       // позиция и размер (будем менять в WM_SIZE)
+            hwnd,
+            (HMENU)1001,            // id контрола
+            (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
+            NULL
+        );
+        // Установим системный GUI-шрифт (иначе текст будет выглядеть по-старинке)
+        HFONT hFont = (HFONT)GetStockObject(DEFAULT_GUI_FONT);
+        SendMessageW(hEdit, WM_SETFONT, (WPARAM)hFont, TRUE);
+
+
         SetTimer(hwnd, 1, 1000, NULL);
         HMENU hMenubar = CreateMenu();
         HMENU hFile = CreateMenu();
@@ -168,6 +246,46 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
     case WM_COMMAND:
         switch (LOWORD(wParam))
         {
+        case ID_FILE_OPEN:
+        {
+            OPENFILENAME ofn;
+            wchar_t szFile[MAX_PATH] = { 0 };
+            ZeroMemory(&ofn, sizeof(ofn));
+            ofn.lStructSize = sizeof(ofn);
+            ofn.hwndOwner = hwnd;
+            ofn.lpstrFile = szFile;
+            ofn.nMaxFile = MAX_PATH;
+            ofn.lpstrFilter = L"Текстовые файлы\0*.txt\0Все файлы\0*.*\0";
+            ofn.nFilterIndex = 1;
+            ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+            if (GetOpenFileNameW(&ofn)) {
+                LoadFileToEdit(hEdit, szFile);
+            }
+            break;
+        }
+        case ID_FILE_SAVE:
+        {
+            OPENFILENAME ofn;
+            wchar_t szFile[MAX_PATH] = { 0 };
+            ZeroMemory(&ofn, sizeof(ofn));
+            ofn.lStructSize = sizeof(ofn);
+            ofn.hwndOwner = hwnd;
+            ofn.lpstrFile = szFile;
+            ofn.nMaxFile = MAX_PATH;
+            ofn.lpstrFilter = L"Текстовые файлы\0*.txt\0Все файлы\0*.*\0";
+            ofn.nFilterIndex = 1;
+            ofn.Flags = OFN_PATHMUSTEXIST | OFN_OVERWRITEPROMPT;
+            if (GetSaveFileNameW(&ofn)) {
+                SaveEditToFile(hEdit, szFile);
+            }
+            break;
+        }
+        case ID_EDIT_CUT:
+            SendMessage(hEdit, WM_CUT, 0, 0); break;
+        case ID_EDIT_COPY:
+            SendMessage(hEdit, WM_COPY, 0, 0); break;
+        case ID_EDIT_PASTE:
+            SendMessage(hEdit, WM_PASTE, 0, 0); break;
         case ID_FILE_EXIT: PostQuitMessage(0); break;
         case ID_HELP_ABOUT: MessageBox(hwnd, L"Простой редактор", L"О программе", MB_OK); break;
         }
@@ -181,7 +299,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
     case WM_TIMER:
         idleSeconds++;
-        if (idleSeconds >= 6 && !sc_showed)
+        if (idleSeconds >= 20 && !sc_showed)
         {
             sc_showed = true;
             idleSeconds = 0;
